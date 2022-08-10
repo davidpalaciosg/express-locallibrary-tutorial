a0232af5ce2bf8a989d39148a412c694db11ab19
Aulas Ingeniería <aulasingenieria@javeriana.edu.co>
2022-08-10 10:42:33 -0500

    Wiki Route created

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose');
 //Routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var wikiRouter = require('./routes/wiki');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/wiki", wikiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
